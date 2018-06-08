864ccf05df193908905fedac767b1374883a02b0
Juan Marcos Susana Feliz <jsusana14@siuben.gob.do>
2018-06-08 08:54:15 -0400

    Init Reportes

--- RentACar/RentACar/Views/SingleCliente.cs
+++ RentACar/RentACar/Views/SingleCliente.cs
@@ -116,8 +116,33 @@ namespace RentACar.Views
         {
             if (currentAction == SaveAction.Agregar) { Add(); }
 
+            if (currentAction == SaveAction.Editar) { Edit(); }
+
             AppData.ViewsRepository.ClientesView.GetData();
             Close();
+        }
+
+        private void Edit()
+        {
+            try
+            {
+                var cliente = _db.Clientes.FirstOrDefault(x => x.Id == currentClient.Id);
+
+                cliente.Nombre = textBox1.Text + " " + textBox2.Text;
+                cliente.Cedula = textBox3.Text;
+                cliente.TipoPersona = (comboBox1.SelectedItem.ToString() == "Persona Física") ? 1 : 2;
+                cliente.NoTarjeta = (textBox4.Text.Length > 16) ? textBox4.Text.Remove(16) : textBox1.Text;
+                cliente.LimiteCredito = decimal.Parse(textBox5.Text);
+                cliente.Deleted = false;
+                cliente.Estado = checkBox1.Checked;               
+
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
 
         private void Add()
